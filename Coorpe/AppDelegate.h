f0f629be0c271a042d7b3617d1791552c80fd78e
TRSturbo <AaronT@Aarons-CrackBook-2.local>
2014-10-02 22:08:00 -0400

    Initial Commit

---
+++ Coorpe/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Coorpe
+//
+//  Created by Aaron Tharpe on 10/2/14.
+//  Copyright (c) 2014 Aaron Tharpe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Coorpe/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Coorpe
+//
+//  Created by Aaron Tharpe on 10/2/14.
+//  Copyright (c) 2014 Aaron Tharpe. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
